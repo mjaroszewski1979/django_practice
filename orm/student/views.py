946de6fa714923f656c4d6f9b1a83ac682ffd13e
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2022-03-02 20:55:51 +0100

    added raw sql

--- orm/student/views.py
+++ orm/student/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Student
+from .models import Student, Teacher
 from django.db import connection
 from django.db.models import Q
 
@@ -22,6 +22,13 @@ def student_list(request):
 
     return render(request, 'index.html',{'posts':posts})'''
 
+def dictfetchall(cursor):
+    desc = cursor.description
+    return [
+        dict(zip([col[0] for col in desc], row))
+        for row in cursor.fetchall()
+    ]
+
 def student_list(request):
     context = {}
     all_studs = Student.objects.all()
@@ -31,6 +38,19 @@ def student_list(request):
     context['data2'] = Student.objects.all()
     context['data3'] = Student.objects.filter(Q(age__gt=30) & Q(age__lt=40))
     context['data4'] = all_studs.exclude(Q(firstname__endswith='ej') & ~Q (classroom=2))
+    context['union'] = Student.objects.all().values_list('firstname').union(Teacher.objects.all().values_list('firstname'))
+    context['union_dict'] = Student.objects.all().values('firstname').union(Teacher.objects.all().values('firstname'))
+    context['only'] = Student.objects.filter(Q(surname__endswith='ski')).only('surname')
+    context['raw'] = Teacher.objects.raw("SELECT * FROM student_student")
+
+    '''cursor = connection.cursor()
+    cursor.execute("SELECT count(*) FROM student_teacher")
+    context['counter'] = cursor.fetchone()'''
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM student_student")
+    context['dict'] = dictfetchall(cursor)
+
 
     print(context['data'].query)
     print(connection.queries)

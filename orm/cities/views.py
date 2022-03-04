42bba48320b58188d4c8248ad1007bbf1338865e
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2022-03-04 22:48:50 +0100

    added aggr and annotate and filtering combined

--- orm/cities/views.py
+++ orm/cities/views.py
@@ -22,7 +22,20 @@ def cities_list(request):
     all_countries = Country.objects.all()
 
     context['avg_all_cities'] = City.objects.filter(Q(country__name=all_countries[3])).aggregate(Avg('population'))
-    context['data'] = all_countries[4]
+
+    context['data'] = Country.objects.annotate(av_pop = Avg('city__population'))
+
+    context['data1'] = Country.objects.filter(Q(name__startswith='un')).annotate(num_cities=Count('city'))
+
+    context['data2'] = Country.objects.filter(Q(name__startswith='un')).aggregate(Min('city__population'))
+
+    context['data3'] = Country.objects.annotate(avg_pop=Avg('city__population')).filter(avg_pop__gt=2000000)
+
+    context['data4'] = Country.objects.annotate(num_cities=Count('city')).aggregate(Min('num_cities'))
+
+    context['data5'] = Country.objects.annotate(num_cities=Count('city', filter=Q(city__population__gt=3000000)))
+
+    context['data6'] = Country.objects.annotate(num_cities=Count('city', filter=Q(city__population__gt=3000000))).order_by('-num_cities')
     
 
     return render(request, 'cities.html',{'context': context})

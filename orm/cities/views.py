09ed35fab75aecce9fbb9c4065ca16b38ab796db
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2022-03-05 22:07:52 +0100

    added comments

--- orm/cities/views.py
+++ orm/cities/views.py
@@ -11,30 +11,34 @@ def cities_list(request):
     context = {}
     cities = City.objects.all()
     context['vars'] = vars(cities[0])
+    # Minimal population for each city
     min_population = City.objects.annotate(Min('population'))
     context['min_population'] = vars(min_population[1])
+    # Average population for all cities
     avg_population = City.objects.aggregate(Avg('population'))
     context['avg_population'] = avg_population
+    # Number of cities per Country
     city_num = Country.objects.annotate(result = Count('city'))
     context['city_num'] = city_num[0].result
+    # Cities with population above average
     above_avg = City.objects.filter(Q(population__gt=avg_population['population__avg']))
     context['above_avg'] = above_avg
     all_countries = Country.objects.all()
-
+    # Average population for cities in country number 3
     context['avg_all_cities'] = City.objects.filter(Q(country__name=all_countries[3])).aggregate(Avg('population'))
-
+    # Average population in cities for particular country
     context['data'] = Country.objects.annotate(av_pop = Avg('city__population'))
-
+    # Number of cities for countries which names starting with un
     context['data1'] = Country.objects.filter(Q(name__startswith='un')).annotate(num_cities=Count('city'))
-
+    # Minimal population in cities located in countries with names starting with un
     context['data2'] = Country.objects.filter(Q(name__startswith='un')).aggregate(Min('city__population'))
-
+    # Average population in cities with population greater than 2 mln
     context['data3'] = Country.objects.annotate(avg_pop=Avg('city__population')).filter(avg_pop__gt=2000000)
-
+    # Minimal number of cities for particular country
     context['data4'] = Country.objects.annotate(num_cities=Count('city')).aggregate(Min('num_cities'))
-
+    # Number of cities for partcicular country with population greater than 3 mln
     context['data5'] = Country.objects.annotate(num_cities=Count('city', filter=Q(city__population__gt=3000000)))
-
+    # Number of cities for partcicular country with population greater than 3 mln in ascending order
     context['data6'] = Country.objects.annotate(num_cities=Count('city', filter=Q(city__population__gt=3000000))).order_by('-num_cities')
     
 
--- orm/student/views.py
+++ orm/student/views.py
@@ -16,20 +16,26 @@ def student_list(request):
     context = {}
     all_studs = Student.objects.all()
     context['all'] = all_studs
+    # Filter for students with surnames starting with and not starting with...
     context['data'] = Student.objects.filter(Q(surname__endswith='ski') & ~Q (surname__startswith='jaro'))
+    # Filter for students with surnames starting with or starting with...
     context['data1'] = Student.objects.filter(surname__startswith='jaroszewski') | Student.objects.filter(surname__startswith='kowalski')
     context['data2'] = Student.objects.all()
     context['data3'] = Student.objects.filter(Q(age__gt=30) & Q(age__lt=40))
     context['data4'] = all_studs.exclude(Q(firstname__endswith='ej') & ~Q (classroom=2))
+    # Performing union of list values - students first names and teachers first names - must be same model field to work
     context['union'] = Student.objects.all().values_list('firstname').union(Teacher.objects.all().values_list('firstname'))
+    # Performing union of values - students first names and teachers first names
     context['union_dict'] = Student.objects.all().values('firstname').union(Teacher.objects.all().values('firstname'))
+    # Filter for students surname endswith ski and returning only their surnames
     context['only'] = Student.objects.filter(Q(surname__endswith='ski')).only('surname')
+    # Raw sql querries
     context['raw'] = Teacher.objects.raw("SELECT * FROM student_student")
 
     '''cursor = connection.cursor()
     cursor.execute("SELECT count(*) FROM student_teacher")
     context['counter'] = cursor.fetchone()'''
-
+    # Using cursor object to perform sql query and returnig dictionary as a result
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM student_student")
     context['dict'] = dictfetchall(cursor)

36f8384d59d16fc060db21826afdcb15988b53db
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2022-03-07 21:16:19 +0100

    added prefetch related

--- orm/products/urls.py
+++ orm/products/urls.py
@@ -5,4 +5,5 @@ app_name = 'products'
 
 urlpatterns = [
     path('', views.home, name='home'),
+
 ]
--- orm/products/views.py
+++ orm/products/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Customer, Product, Order, LineItem
 from django.db import connection
-from django.db.models import Q, Min, Avg, Count, Sum, F
+from django.db.models import Q, Min, Avg, Count, Sum, F, Value
+from django.db.models.functions import Concat
 from datetime import timedelta
 
 
@@ -15,7 +16,15 @@ def home(request):
     context['average'] = LineItem.objects.aggregate(average=Avg(F('product__price') * F('quantity')))
     context['dates_match'] = Order.objects.filter(order_date=F('delivered_date')).order_by('order_date')
     context['del_time'] = Order.objects.annotate(del_time=F('delivered_date') - F('order_date')).filter(del_time__lte=timedelta(days=2)).filter(Q(customer__email__startswith='j') & Q(customer__last_name__endswith='ski'))
+    context['concat'] = Customer.objects.annotate(full_name=Concat('first_name', Value(" "), 'last_name')).filter(full_name__icontains='ja')
+    # For models with foreign key
+    context['orders'] = Order.objects.select_related('customer').all().filter(Q(customer__city__startswith='l'))
+    # For models with many to many relationship
+    context['products'] = Order.objects.prefetch_related('products').all().filter(products__price__gt=500)
 
-    
+ 
 
     return render(request, 'home.html',{'context': context})
+
+
+
